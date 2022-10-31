4396f555a56afaf7e35ecbf63167b764512bcef7
MoritzOtterbein <moritz.otterbein@informatik.hs-fulda.de>
2022-10-31 22:22:47 +0100

    Test

--- manipulate.js
+++ manipulate.js
@@ -1,20 +1,3 @@
-'use strict';
-document.addEventListener('DOMContentLoaded', function () {
-	let text1 = 'Dies ist ein neuer Absatz!';
-	let text2 = 'Dies ist ein neuer Button!';
-	document.getElementById('text')
-		.addEventListener('click', function () {
-			
-		});
-	document.getElementById('button2')
-		.addEventListener('click', function () {
-			createElement('#textblock', 'button', text2);
-		});
-
-	function createElement(parent, elem, content) {
-		let container = document.querySelector(parent);
-		let newElm = document.createElement(elem);
-		newElm.innerText = content;
-		container.appendChild(newElm);
-	}
-});
+document.getElementById().onclick = function() {
+	var vorname = document.getElementById("vorname")
+}
