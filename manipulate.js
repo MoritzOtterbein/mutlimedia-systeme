cceaad93574fed0a065a9e34657df4aed363ddcc
MoritzOtterbein <moritz.otterbein@informatik.hs-fulda.de>
2022-11-01 10:21:13 +0100

    main

--- manipulate.js
+++ manipulate.js
@@ -1,3 +1,9 @@
-document.getElementById().onclick = function() {
-	var vorname = document.getElementById("vorname")
-}
+
+window.onload = function(){ 
+    document.getElementById("myButton").onclick = function(){
+	
+		var vorname = document.getElementById("myVorname").value;
+		console.log("Hello",vorname);
+	
+	}
+};
