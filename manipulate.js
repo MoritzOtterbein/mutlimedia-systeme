20c5675f4d76839087a7cb3ebb3579b35017ea90
MoritzOtterbein <moritz.otterbein@informatik.hs-fulda.de>
2022-11-01 13:53:03 +0100

    Läuft

--- manipulate.js
+++ manipulate.js
@@ -2,8 +2,31 @@
 window.onload = function(){ 
     document.getElementById("myButton").onclick = function(){
 	
-		var vorname = document.getElementById("myVorname").value;
-		console.log("Hello",vorname);
-	
+		let vorname = document.getElementById("myVorname").value;
+		let ceasarKey = document.getElementById("ceasarKey").value;
+		let vornameLenght = vorname.length;
+		//console.log(vornameLenght);
+		//Text to lower Case, output
+		
+		let output = document.getElementById("output");
+		vorname = vorname.toLowerCase();
+		let inhalt = vorname.value;
+		let alphaArr = [];
+		output.innerHTML = vorname;
+		runThrough(vorname, vornameLenght);
+		c = vorname.charCodeAt(0)
+		console.log(ceasarKey);
+		
 	}
+
+
 };
+function runThrough(vorname, vornameLenght) {
+	
+	console.log(vornameLenght);
+	for (let index = 0; index < vornameLenght-1; index++) {
+		console.log(vorname.charCodeAt(index));
+		
+	}
+	
+}
