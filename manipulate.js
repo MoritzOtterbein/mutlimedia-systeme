2175276427445f33624f83db88b5dfde12651f37
MoritzOtterbein <moritz.otterbein@informatik.hs-fulda.de>
2022-11-02 12:51:16 +0100

    1.2

--- manipulate.js
+++ manipulate.js
@@ -2,20 +2,20 @@
 window.onload = function(){ 
     document.getElementById("myButton").onclick = function(){
 	
-		let vorname = document.getElementById("myVorname").value;
+		let eingabe = document.getElementById("myVorname").value;
 		let ceasarKey = document.getElementById("ceasarKey").value;
-		let vornameLenght = vorname.length;
+		let eingabeLänge = eingabe.length;
 		//console.log(vornameLenght);
 		//Text to lower Case, output
 		
 		let output = document.getElementById("output");
-		vorname = vorname.toLowerCase();
-		let inhalt = vorname.value;
+		eingabe = eingabe.toLowerCase();
+		let inhalt = eingabe.value;
 		//Array zum Buchstaben zählen
-		let alphaArr = [];
-		output.innerHTML = vorname;
-		runThrough(vorname, vornameLenght, alphaArr);
-		c = vorname.charCodeAt(0)
+		let alphaArr = new Array(26).fill(0);
+		output.innerHTML = eingabe;
+		runThrough(eingabe, eingabeLänge, alphaArr);
+		c = eingabe.charCodeAt(0)
 		
 		
 	}
@@ -25,11 +25,10 @@ window.onload = function(){
 function runThrough(vorname, vornameLenght, alphaArr) {
 	
 	console.log(vornameLenght);
-	for (let index = 0; index < vornameLenght-1; index++) {
-		let cacheAlpha = vorname.charCodeAt(index) - 97;
-		console.log(cacheAlpha);
-		alphaArr[cacheAlpha] +=1;
+	for (let index = 0; index < vornameLenght; index++) {
+		let  cacheAlpha = vorname.charCodeAt(index) - 97;
 		
+		alphaArr[parseInt(cacheAlpha)] +=1;
 		console.log(alphaArr);
 	}
 	
