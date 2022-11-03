ec47bbf4935a2335c387e1106400fd459ab89392
MoritzOtterbein <moritz.otterbein@informatik.hs-fulda.de>
2022-11-04 00:03:02 +0100

    jo

--- manipulate.js
+++ manipulate.js
@@ -1,35 +1,79 @@
 
-window.onload = function(){ 
-    document.getElementById("myButton").onclick = function(){
-	
-		let eingabe = document.getElementById("myVorname").value;
+window.onload = function () {
+	//Umwandeln mit Key
+	document.getElementById("myButton").onclick = function () {
+		let entry = document.getElementById("text").value;
+
+	}
+	//Umwandeln ohne Key
+	document.getElementById("decodeKEY").onclick = function () {
+
+		let entry = document.getElementById("text").value;
 		let ceasarKey = document.getElementById("ceasarKey").value;
-		let eingabeLänge = eingabe.length;
-		//console.log(vornameLenght);
-		//Text to lower Case, output
-		
 		let output = document.getElementById("output");
-		eingabe = eingabe.toLowerCase();
-		let inhalt = eingabe.value;
+		let findKey = 0;
+		let inhalt = entry.value;
 		//Array zum Buchstaben zählen
 		let alphaArr = new Array(26).fill(0);
-		output.innerHTML = eingabe;
-		runThrough(eingabe, eingabeLänge, alphaArr);
-		c = eingabe.charCodeAt(0)
-		
-		
+		output.innerHTML = entry;
+		findKey = catchKey(entry, alphaArr);
+		converter(entry);
+
+
+	}
+	/*To-DO: Code einlesen
+	Groß Buchstaben und Kleinbuchstaben
+	ASCII umwandeln
+	Buchstaben zurück umwandeln
+	Ausgeben
+	*/
+
+	function converter(entry) {
+		console.log(entry.length);
+		let ascii = new Array(entry.length).fill(0);
+		for (let i = 0; i < entry.length; i++) {
+			ascii[i] = entry.charCodeAt(i);
+		}
 	}
 
 
 };
-function runThrough(vorname, vornameLenght, alphaArr) {
-	
-	console.log(vornameLenght);
-	for (let index = 0; index < vornameLenght; index++) {
-		let  cacheAlpha = vorname.charCodeAt(index) - 97;
-		
-		alphaArr[cacheAlpha] +=1;
-		console.log(alphaArr);
+function catchKey(entry, alphaArr, key) {
+
+	console.log(entry.length);
+
+	for (let i = 0; i < entry.length; i++) {
+		let cacheAlpha = entry.charCodeAt(i) - 97;
+
+		alphaArr[cacheAlpha] += 1;
+
 	}
-	
+	for (let i = 0; i < alphaArr.length; i++) {
+		;
+		let key = 0;
+		if (alphaArr[i] > key) {
+			key = i;
+			console.log(key);
+		}
+		return key;
+
+	}
+	function shiften(entry, key) {
+		converter(entry);
+		for (let i = 0; i < entry.length; i++) {
+			if (entry[i] < 123 && entry[i] > 96) {
+				entry[i] = entry[i] + key;
+				if (entry[i] > 122) {
+					entry[i] = entry[i] % 122 + 96;
+				}
+			}
+			else if (entry[i] < 91 && entry[i] > 64) {
+				entry[i] = entry[i] + key;
+				if (entry[i] > 90) {
+					entry[i] = entry[i] % 90 + 65;
+				}
+			}
+
+		}
+	}
 }
